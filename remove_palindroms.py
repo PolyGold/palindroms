0a969e626b0443b4516fbbb0ab838c93f08c3267
polyg <polygold@yandex.ru>
2020-10-22 16:33:26 +0300

    123

--- remove_palindroms.py
+++ remove_palindroms.py
@@ -1,7 +1,5 @@
 def remove_palindroms(spells):
-    d = 0
     for i in spells:
-        d += 1
         if i.lower() == i[::-1].lower():
-            spells.pop(d - 1)
+            spells.remove(spell)
     return spells
